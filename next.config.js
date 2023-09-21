2cccf86a41f7204092fd8b945c51110abe396eeb
Pudnaa <143073054+interactive-portal@users.noreply.github.com>
2023-09-21 11:57:06 +0800

    update
    config

--- next.config.js
+++ next.config.js
@@ -33,9 +33,7 @@ const nextConfig = {
     minimumCacheTTL: 60,
     disableStaticImages: true,
   },
-  experimental: {
-    serverActions: true,
-  },
+
   // experimental: {
   //   externalDir: true,
   //   typedRoutes: true,
