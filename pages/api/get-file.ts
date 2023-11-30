a727dbf43de50b5c27de2d8b60f2bcb08619d285
Pudnaa <34536859+Pudnaa@users.noreply.github.com>
2023-11-30 18:33:30 +0800

    dd
    dd

--- pages/api/get-file.ts
+++ pages/api/get-file.ts
@@ -1,7 +1,6 @@
 import axios from "axios";
 import { NextApiRequest, NextApiResponse } from "next";
 import { Readable } from "stream";
-import { metaConfig } from "../../config/metaConfig";
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
   const filename = req.query.param;
